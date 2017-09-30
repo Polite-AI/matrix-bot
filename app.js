f031719d3689f37e980f10ba39732e61f6c6a778
Joseph Reeve <joseph.reeve@googlemail.com>
2017-09-30 13:54:23 +0100

    A few changes

--- app.js
+++ app.js
@@ -1,16 +1,25 @@
 const log = require('./logger.js');
+const request = require('request-promise-native');
 
 module.exports = function (message, room, event, client) {
     log.log(`Recieved message [${message}]`);
-    const isAngry = ~message.toLowerCase().indexOf('angry');
-
-    if (isAngry) {
-        log.log(`Found an 'angry' message: [${message}]`);
-        return new Promise((resolve, reject) => {
-            client.sendTextMessage(room.roomId, `${event.getSender()}: Don't be angry... We're all friends here...`);
-            resolve();
+    return request.post({
+            method: 'POST',
+            uri: 'http://api.polite.ai/api/1.0/classify',
+            body: {
+                text: message
+            },
+            json: true
+        })
+        .then(res => {
+            log.log(`Got following results for [${message}]: `, res.results);
+            const positiveResults = Object.keys(res.results).filter(key => Number(res.results[key]));
+            if (positiveResults.length) {
+                const scoreText = Object.keys(res.results).map(key => `${key}=${res.results[key]}`).join(', ');
+                client.sendTextMessage(room.roomId, `${event.getSender()}: Your message was detected as the following: ${scoreText}`);
+            }
+        })
+        .catch(err => {
+            log.warn(`Failed to get data from api (${message})`, err);
         });
-    }
-
-    return Promise.resolve();
 };
