f89751a41be3bbbf9bfbfcc6c237cf74a7e6f342
Joseph Reeve <joseph.reeve@googlemail.com>
2017-09-30 16:41:23 +0100

    Progress

--- config.example.js
+++ config.example.js
@@ -1,13 +1,39 @@
 // For help getting USERNAME and ACCESSTOKEN, see this article: https://reeve.xyz/getting-started-with-matrix-bots-on-nodejs/
 
 const config = {
-    "production": {},
-    "development": {
-        "matrix": {
+    "production": {
+        "api": {
+            "version": "1.0"
+        },
+        "postgres": {
+            "host": "HOST",
+            "user": "USER",
+            "password": "PASSWORD",
+            "database": "DATABASE"
+        },
+        "bots": [{
             "baseUrl": "https://matrix.org",
             "userId": "@USERNAMEHERE:matrix.org",
-            "accessToken": "ACCESSTOKENHERE"
-        }
+            "accessToken": "ACCESSTOKENHERE",
+            "language": "english"
+        }]
+    },
+    "development": {
+        "api": {
+            "version": "1.0"
+        },
+        "postgres": {
+            "host": "HOST",
+            "user": "USER",
+            "password": "PASSWORD",
+            "database": "DATABASE"
+        },
+        "bots": [{
+            "baseUrl": "https://matrix.org",
+            "userId": "@USERNAMEHERE:matrix.org",
+            "accessToken": "ACCESSTOKENHERE",
+            "language": "english"
+        }]
     }
 };
 
