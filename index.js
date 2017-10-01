1cb12a3c07674f678ed77bed18f9c54807671415
rjp44 <rob@ipcortex.co.uk>
2017-10-01 12:25:20 +0100

    revert my breakage

--- index.js
+++ index.js
@@ -62,11 +62,11 @@ config.bots.forEach(botConfig => {
         if (member.membership === "invite" && member.userId === botConfig.userId) {
             client.joinRoom(member.roomId).done(function () {
                 log.log("Auto-joined %s", member.roomId);
-                const room_key = require('crypto').createHash('sha256',salt)
-                                    .update('matrix'+room.roomId)
-                                    .digest('base64');
-                client.sendTextMessage(member.roomId, `Polite.AI bot has entered the building...\nTeach me more at http://api.polite.ai/admin/`+room_key);
-                
+                //const room_key = require('crypto').createHash('sha256',salt)
+                //                    .update('matrix'+room.roomId)
+                //                    .digest('base64');
+                client.sendTextMessage(member.roomId, 'Polite.AI bot has entered the building...\nTeach me more at http://api.polite.ai/admin/');
+
             });
         }
     });
