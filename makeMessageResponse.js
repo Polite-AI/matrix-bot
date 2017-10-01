a8c21fc2e3e15a17ecbf4982e1450b17bf0a5dbc
rjp44 <rob@ipcortex.co.uk>
2017-10-01 10:08:51 +0100

    add message anchor back into challenge URL

--- makeMessageResponse.js
+++ makeMessageResponse.js
@@ -4,7 +4,7 @@ const myURL = 'http://api.polite.ai';
 module.exports = function makeMessageResponse(message, classification, language, key, eventid) {
     const pack = languagePacks[language || 'english'];
     const randIndex = Math.floor(Math.random() * pack.length);
-    const reportUrl = ' (challenge me at '+myURL+'/admin/'+key+')';
-    //const reportUrl = ' (challenge me at '+myURL+'/admin/'+key+'#'+eventid+')';
+    //const reportUrl = ' (challenge me at '+myURL+'/admin/'+key+')';
+    const reportUrl = ' (challenge me at '+myURL+'/admin/'+key+'#'+encodeURIComponent(eventid)+')';
     return pack[randIndex]+reportUrl;
 }
