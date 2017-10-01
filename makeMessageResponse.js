5c7d0df3aeb7927be701385df8a08c2f768c9e80
rjp44 <rob@ipcortex.co.uk>
2017-10-01 12:15:56 +0100

    better intro

--- makeMessageResponse.js
+++ makeMessageResponse.js
@@ -5,6 +5,9 @@ module.exports = function makeMessageResponse(message, classification, language,
     const pack = languagePacks[language || 'english'];
     const randIndex = Math.floor(Math.random() * pack.length);
     //const reportUrl = ' (challenge me at '+myURL+'/admin/'+key+')';
-    const reportUrl = ' (challenge me at '+myURL+'/admin/'+key+((eventid != null)?('#'+encodeURIComponent(eventid)):'')+')';
+    var reportUrl = ' (challenge me at '+myURL+'/admin/'+key;
+    if (eventid != null)
+        reportURL += '#'+encodeURIComponent(eventid);
+    reportUrl += ')';
     return ((classification != null)?pack[randIndex]:message)+reportUrl;
 }
